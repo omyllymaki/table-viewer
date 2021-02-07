98d7e1d828a8360f9bfc84eb83a608d97479e644
Ossi Myllymäki <omyllymaki@gmail.com>
2021-02-07 22:02:32 +0200

    Add requirements and update documentation

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # table-viewer
-Simple GUI for viewing and filtering table data.
+
+Simple utility program for viewing, filtering, grouping, and sorting of tabular data.
+
+## Usage
+
+Basic usage:
+
+```
+python main.py --path path1 path2 path3 
+``` 
+
+or simply just
+
+```
+python main.py 
+``` 
+
+For more detailed usage, see
+
+```
+python main.py -h
+```
+
+**Queries**
+
+
+Syntax:
+
+* [col1, col2]: search from col1 and col2.
+* &&: logical AND, returns intersection.
+* ||: logical OR, returns union.
+
+Example:
+
+[col1, col2] regxp1 && regxp2 || [col3] regexp3
+
+See query_examples.py file for more examples.
+
--- parsing.py
+++ parsing.py
@@ -6,11 +6,10 @@ import os
 def init_parser():
     parser = argparse.ArgumentParser(description='Read in a file or set of files, and return the result.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-p', '--path', default=None, nargs='+', help='Path of a file or a folder of files.')
-    parser.add_argument('-e', '--extension', default=None, help='File extension to filter by.')
-    parser.add_argument('-r', '--recursive', action='store_true', default=False, help='Search through sub folders.')
-    parser.add_argument('-s', '--separator', default=None,
-                        help="Delimiter to use. If none, it is detected automatically.")
+    parser.add_argument('-p', '--path', default=None, nargs='+', help='File paths or folder of files. File dialog is openened if None (default value).')
+    parser.add_argument('-e', '--extension', default=None, help='File extension to filter by. By defauilt None which means no filtering by extension.')
+    parser.add_argument('-r', '--recursive', action='store_true', default=False, help='Make recursive search through sub folders.')
+    parser.add_argument('-s', '--separator', default=None, help="Delimiter to use. If none, it is detected automatically.")
     parser.add_argument('-d', '--decimal', default=".", help="Decimal to use.")
     parser.add_argument('-hr', '--header', type=int, default=0, help="Row number(s) to use as the column names.")
     parser.add_argument('-dt', '--datetime', default=None, nargs='+', help='Datetime column names.')
